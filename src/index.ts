8db4f9b44a5dd96ab7309a21f5aa4a453a772708
David Bowland <dbowland1@gmail.com>
2021-05-18 00:59:05 -0500

    Set git author for workflow

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,2 @@
-// Output debug info
-console.log('Package name', process.env.npm_package_name)
-console.log('Package version', process.env.npm_package_version)
-
 // Import modules to create resources
 import './iam/index'
